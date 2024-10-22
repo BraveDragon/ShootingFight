7850e7b55fd71a812bba4b2e6849fb916672b910
BraveDragon <masa010515@hera.eonet.ne.jp>
2024-10-22 17:58:20 +0900

    画像読み込みの高速化

--- Resources.py
+++ Resources.py
@@ -3,11 +3,12 @@ import Objects
 
 #ゲームに使用する素材類を一括で管理するクラス
 class Resources:
-    player1 = pygame.image.load("Resources/GunPoint1P.png")
-    player2 = pygame.image.load("Resources/GunPoint2P.png")
-    ufo = pygame.image.load("Resources/UFO.png")
-    #そのまま読みこむとUFOの画像は大きすぎるので、ここで縮小する
-    ufo = pygame.transform.scale(ufo,[Objects.UFO_WIDTH,Objects.UFO_HEIGHT])
-    #そのまま読みこむとエイリアンの画像は大きすぎるので、ここで縮小する
-    alien = pygame.image.load("Resources/Alien.png")
-    alien = pygame.transform.scale(alien,[Objects.ALIEN_WIDTH,Objects.ALIEN_HEIGHT])
+    def __init__(self):
+        self.player1 = pygame.image.load("Resources/GunPoint1P.png").convert_alpha()
+        self.player2 = pygame.image.load("Resources/GunPoint2P.png").convert_alpha()
+        ufo = pygame.image.load("Resources/UFO.png").convert_alpha()
+        #そのまま読みこむとUFOの画像は大きすぎるので、ここで縮小する
+        self.ufo = pygame.transform.scale(ufo,[Objects.UFO_WIDTH,Objects.UFO_HEIGHT]).convert_alpha()
+        #そのまま読みこむとエイリアンの画像は大きすぎるので、ここで縮小する
+        alien = pygame.image.load("Resources/Alien.png").convert_alpha()
+        self.alien = pygame.transform.scale(alien,[Objects.ALIEN_WIDTH,Objects.ALIEN_HEIGHT]).convert_alpha()
