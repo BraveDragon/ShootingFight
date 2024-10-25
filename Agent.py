8adcab566b70e2e851508bbb9db4044ea48c6b7f
BraveDragon <masa010515@hera.eonet.ne.jp>
2024-10-25 18:59:12 +0900

    最終出力の形状が(1, 行動数)の配列になるように修正

--- Agent.py
+++ Agent.py
@@ -13,9 +13,9 @@ class Agent(nn.Module):
         self.rrelu = nn.RReLU()
         self.pool = nn.MaxPool2d(2, stride=2)
         self.conv1 = nn.Conv2d(3, 3, 16)
-        self.conv2 = nn.Conv2d(3, 16, 16)
-        self.fc1 = nn.Linear(7,16)
-        self.fc2 = nn.Linear(16, Outputs)
+        self.conv2 = nn.Conv2d(3, 1, 16)
+        self.fc1 = nn.Linear(91,32)
+        self.fc2 = nn.Linear(32, Outputs)
     
     def forward(self, x):
         x = self.conv1(x)
@@ -24,6 +24,7 @@ class Agent(nn.Module):
         x = self.conv2(x)
         x = self.rrelu(x)
         x = self.pool(x)
+        x = x.view(x.size()[0], -1)
         x = self.fc1(x)
         x = self.rrelu(x)
         x = self.fc2(x)
